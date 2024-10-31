570b03f9e86087d2454e38f239aae13fbb0231f7
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-10-31 20:04:55 +0900

    Initial Commit

---
+++ WorkWave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkWave
+//
+//  Created by 조규연 on 10/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+//
+//#Preview {
+//    ContentView()
+//}
---
+++ WorkWave/WorkWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWaveApp.swift
+//  WorkWave
+//
+//  Created by 조규연 on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
