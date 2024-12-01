9ae127e6d9219f5cfa9d66121ca4400d2f58fc30
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-12-02 07:18:16 +0900

    워크스페이스 리스트 구성요소 프로퍼티로 분리

--- WorkWave/WorkSpace/TabView/List/WorkspaceListView.swift
+++ WorkWave/WorkSpace/TabView/List/WorkspaceListView.swift
@@ -10,47 +10,15 @@ import ComposableArchitecture
 
 struct WorkspaceListView: View {
     @Bindable var store: StoreOf<WorkspaceList>
-    
+
     var body: some View {
         VStack {
-            HStack {
-                Text("워크스페이스")
-                    .applyFont(font: .title1)
-                    .padding(.horizontal)
-                    .padding(.top, 60)
-                    .padding(.bottom, 8)
-                
-                Spacer()
-            }
-            .background(.primaryBackground)
+            headerView
             
             if store.workspaces.isEmpty {
-                VStack(spacing: 20) {
-                    Spacer()
-                    
-                    Text("워크스페이스를 찾을 수 없어요.")
-                        .applyFont(font: .title1)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                    
-                    Text("관리자에게 초대를 요청하거나, 다른 이메일로 시도하거나 새로 워크스페이스를 생성해주세요.")
-                        .applyFont(font: .bodyRegular)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                    
-                    CustomButton(title: "워크스페이스 생성", font: .title2, titleColor: .white, tintColor: .brandGreen) {
-                        store.send(.addWorkspaceTapped)
-                    }
-                    
-                    Spacer()
-                }
-                .padding(.horizontal)
+                emptyListView
             } else {
-                List(store.workspaces, id: \.id) { workspace in
-                    Text(workspace.name)
-                        .applyFont(font: .bodyBold)
-                        .padding(.vertical, 8)
-                }
+                listView
             }
         }
         .sheet(isPresented: $store.isAddWorkspacePresented) {
@@ -61,6 +29,50 @@ struct WorkspaceListView: View {
         }
         .onAppear {
             store.send(.fetchWorkspaces)
+        }
+    }
+    
+    var headerView: some View {
+        HStack {
+            Text("워크스페이스")
+                .applyFont(font: .title1)
+                .padding(.horizontal)
+                .padding(.top, 60)
+                .padding(.bottom, 8)
+            
+            Spacer()
+        }
+        .background(.primaryBackground)
+    }
+    
+    var emptyListView: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("워크스페이스를 찾을 수 없어요.")
+                .applyFont(font: .title1)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            Text("관리자에게 초대를 요청하거나, 다른 이메일로 시도하거나 새로 워크스페이스를 생성해주세요.")
+                .applyFont(font: .bodyRegular)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            CustomButton(title: "워크스페이스 생성", font: .title2, titleColor: .white, tintColor: .brandGreen) {
+                store.send(.addWorkspaceTapped)
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+    
+    var listView: some View {
+        List(store.workspaces, id: \.id) { workspace in
+            Text(workspace.name)
+                .applyFont(font: .bodyBold)
+                .padding(.vertical, 8)
         }
     }
 }
