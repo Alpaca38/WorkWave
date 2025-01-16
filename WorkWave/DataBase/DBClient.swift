e386b92ce670086e220a27afa1b6f225fb12afd5
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2025-01-16 22:30:30 +0900

    DB Client 구현

--- WorkWave/DataBase/DBClient.swift
+++ WorkWave/DataBase/DBClient.swift
@@ -11,11 +11,43 @@ import RealmSwift
 
 @DependencyClient
 struct DBClient {
+    var printRealm: () -> Void
     
+    var createDMChatting: @Sendable (String, DMChattingDBModel) throws -> Void
+    var fetchDMRoom: @Sendable (String) throws -> DMRoomDBModel?
+    
+    var removeAll: @Sendable () throws -> Void
 }
 
 extension DBClient: DependencyKey {
-    static let liveValue = Self()
+    static let liveValue = Self(
+        printRealm: {
+            print(Realm.Configuration.defaultConfiguration.fileURL ?? "No realmURL")
+        },
+        createDMChatting: { roomID, object in
+            let realm = try Realm()
+            
+            guard let dmRoom = realm.object(ofType: DMRoomDBModel.self, forPrimaryKey: roomID) else {
+                print("No DMRoom")
+                return
+            }
+            
+            try realm.write {
+                dmRoom.chattings.append(object)
+            }
+        },
+        fetchDMRoom: { roomID in
+            let realm = try Realm()
+            
+            return realm.object(ofType: DMRoomDBModel.self, forPrimaryKey: roomID)
+        },
+        removeAll: {
+            let realm = try Realm()
+            try realm.write {
+                realm.deleteAll()
+            }
+        }
+    )
 }
 
 extension DependencyValues {
